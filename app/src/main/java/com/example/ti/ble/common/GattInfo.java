215b2d2827f00acf5561687ba5e089a9da07d6f7
Hott-J <jhj13062004@naver.com>
2020-07-03 13:51:18 +0900

    Update GattInfo.java

--- app/src/main/java/com/example/ti/ble/common/GattInfo.java
+++ app/src/main/java/com/example/ti/ble/common/GattInfo.java
@@ -17,7 +17,7 @@ public class GattInfo {
   private static final String uuidTiBase = "f000****-0451-4000-b000-000000000000";
 
   public static final UUID OAD_SERVICE_UUID = UUID.fromString("f000ffc0-0451-4000-b000-000000000000");
-  public static final UUID CC_SERVICE_UUID = UUID.fromString("f000ccc0-0451-4000-b000-000000000000"); //
+  public static final UUID CC_SERVICE_UUID = UUID.fromString("f000ccc0-0451-4000-b000-000000000000"); //MUST NEED? 
 
   private static Map<String, String> mNameMap = new HashMap<String, String>();
   private static Map<String, String> mDescrMap = new HashMap<String, String>();
